94b22253d49e85a288f57d1a851f48f124fd0727
Akanksha Solankurkar <167904936+Akanksha4554@users.noreply.github.com>
2024-08-06 02:11:42 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+let navbar = document.querySelector(".header .navbar");
+
+document.querySelector("#menu-btn").onclick = () => {
+  navbar.classList.toggle("active");
+};
+
+window.onscroll = () => {
+  navbar.classList.remove("active");
+  searchBtn.classlist.toggle("fa-times");
+};
+
+searchBtn.addEventListener("click", () => {
+  searchBtn.classlist.toggle("fa-times");
+  searchBtn.classlist.toggle("active");
+});
+
+document
+  .querySelectorAll(".about .video-container .controls .control-btn")
+  .forEach((btn) => {
+    btn.onclick = () => {
+      let src = btn.getAttribute("data-src");
+      document.querySelector(".about .video-container .video").src = src;
+    };
+  });
